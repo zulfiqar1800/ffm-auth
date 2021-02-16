d37d044b0903acaebae29a6fddef4f638d38feb6
Shahed Mehbub <shahed739@gmail.com>
2021-02-17 01:27:21 +0600

    fixed readme issue

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 # manage.py
 import os
 
-from flask_script import Manager, Server
+from flask_script import Manager, Server, Command, Option
 from flask_migrate import Migrate, MigrateCommand
 
 from project.server import app, db, models
@@ -9,7 +9,60 @@ from project.server import app, db, models
 
 migrate = Migrate(app, db)
 manager = Manager(app)
-manager.add_command("runserver", Server(host='0.0.0.0', port='8000'))
+
+class GunicornServer(Command):
+
+    description = 'Run the app within Gunicorn'
+
+    def __init__(self, host='0.0.0.0', port=8000, workers=4):
+        self.port = port
+        self.host = host
+        self.workers = workers
+
+    def get_options(self):
+        return (
+            Option('-H', '--host',
+                   dest='host',
+                   default=self.host),
+
+            Option('-p', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+
+            Option('-w', '--workers',
+                   dest='workers',
+                   type=int,
+                   default=self.workers),
+        )
+
+    def __call__(self, app, host, port, workers):
+
+        from gunicorn import version_info
+
+        if version_info < (0, 9, 0):
+            from gunicorn.arbiter import Arbiter
+            from gunicorn.config import Config
+            arbiter = Arbiter(Config({'bind': "%s:%d" % (host, int(port)),'workers': workers}), app)
+            arbiter.run()
+        else:
+            from gunicorn.app.base import Application
+
+            class FlaskApplication(Application):
+                def init(self, parser, opts, args):
+                    return {
+                        'bind': '{0}:{1}'.format(host, port),
+                        'workers': workers 
+                    }
+
+                def load(self):
+                    return app
+
+            FlaskApplication().run()
+
+
+manager.add_command("runserver", GunicornServer(host='0.0.0.0', port=8000))
+# manager.add_command("runserver", Server(host='0.0.0.0', port='8000'))
 # migrations
 manager.add_command('db', MigrateCommand)
 
